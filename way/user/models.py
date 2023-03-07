d51b8b3479d6ca05742d15293b55cf8a0559404d
Ajnthony <66324186+Ajnthony@users.noreply.github.com>
2023-03-07 13:55:37 -0800

    user model, serializers and view and .env

---
+++ way/user/models.py
@@ -0,0 +1,62 @@
+import random
+from django.conf import settings
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+from django.utils.translation import gettext_lazy as _
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_('Email is required'))
+    
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+    
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_admin', True)
+        extra_fields.setdefault('is_superuser', True)
+        
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('superuser must have is_superuser: True')
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('superuser must have is_staff: True')
+        
+        user = self.create_user(email, **extra_fields)
+        user.save(using=self._db)
+        
+        return user
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    genders = ['women','men']
+    gender = random.choice(genders)
+    random_number = random.choice(range(1, 100))
+    
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    display_name = models.CharField(max_length=30)
+    email = models.EmailField(_('Email address'))
+    profile_url = models.ImageField(default=f'https://randomuser.me/api/portraits/{gender}/{random_number}.jpg', null=True)
+    bio = models.TextField(max_length=200)
+    address = {
+        'address_line_1': models.TextField(max_length=50),
+        'address_line_2': models.TimeField(max_length=20, null=True),
+        'state':models.CharField(max_length=2),
+        'country':models.TextField(max_length=20),
+        'zip_code': models.CharField(max_length=5),
+    }
+    is_active = models.BooleanField(default=True) # email validation later?
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_moderator = models.BooleanField(default=False)
+    
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
